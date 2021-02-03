331ba1ed26ec15a790a5d622814c60fa528a0f7a
greenhornet <HoneyIsMoney@protonMail.com>
2021-02-03 18:25:12 +0400

    add tenderly

--- scripts/vest.js
+++ scripts/vest.js
@@ -3,7 +3,7 @@ const date = require("date.js");
 const web3 = require("web3");
 const seed = require("./lists/seedList");
 const airdrop = require("./lists/airdropList");
-const tokenManager = "0xAEC5cCf43E481C98FE4975659898083D01682542";
+const tokenManager = "0x2a62Bb210B2a84D2e4b96E9649b976b774F40738";
 
 // helpers
 const toWei = (num) => web3.utils.toWei(num.toString());
@@ -20,7 +20,7 @@ const vestAirdrop = async (batchVest) => {
 	const start = Math.round(date("now").getTime() / 1000);
 	const cliff = Math.round(date("30 days from now").getTime() / 1000);
 	const vesting = Math.round(date("90 days from now").getTime() / 1000);
-	const tranche = chunkArray(airdrop, 40);
+	const tranche = chunkArray(airdrop, 20);
 
 	while (tranche.length > 0) {
 		const chunk = tranche.pop();
@@ -44,7 +44,7 @@ const vestSeed = async (batchVest) => {
 	const cliff = Math.round(date("3 months from now").getTime() / 1000);
 	const vesting = Math.round(date("1 year from now").getTime() / 1000);
 
-  console.log("Seed Vesting");
+	console.log("Seed Vesting");
 	await batchVest.vest(
 		seed.map((user) => user[0]),
 		seed.map((user) => toWei(user[1])),
@@ -59,6 +59,14 @@ async function main() {
 	const BatchVest = await hre.ethers.getContractFactory("BatchVest");
 	const batchVest = await BatchVest.deploy(tokenManager);
 	await batchVest.deployed();
+	await tenderly.verify({
+        name: "BatchVest",
+        address: batchVest.address
+	})
+	await tenderly.push({
+        name: "BatchVest",
+        address: batchVest.address
+    })
 	console.log("batchVest deployed to:", batchVest.address);
 
 
